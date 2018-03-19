d5e66adb117d885aeb1b58ce762085b346ff6e8e
gustavo <gustavohks2@gmail.com>
2018-03-19 15:24:06 -0300

    Finishes main functionality of handling tasks :sparkles:

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -25,9 +25,38 @@
   }
 
   const renderTasks = function(tBodyElement, tasks = []) {
-    // code...
+    tBodyElement.innerHTML = tasks.map((task, i) => `
+        <tr>
+          <td class="cell-task-name">${task.taskName}</td>
+          <td class="cell-pom-count">${task.pomodoroDone} / ${task.pomodoroCount} pomodoris</td>
+          <td class="cell-pom-controls">
+          ${task.pomodoroFinished ? 'Finished |' : `
+            <button class="js-task-done" data-id="${i}">Done</button>
+            <button class="js-increase-pomodoro" data-id="${i}">+1 Pom</button> 
+          `}
+            <button class="js-delete-task" data-id="${i}">Delete Task</button>
+          </td>
+        </tr>
+      `
+    ).join("\n");
   }
 
+  const handleTaskButtonClick = function(evt) {
+    const elClassList = evt.target.classList;
+    const elTaskId = evt.target.getAttribute("data-id");
+
+    elClassList.contains("js-task-done") ? finishTask(tasks, elTaskId) :
+    elClassList.contains("js-increase-pomodoro") ? increasePomodoro(tasks, elTaskId):
+    elClassList.contains("js-delete-task") ? deleteTask(tasks, elTaskId) : null;
+
+    renderTasks(pomodoroTableBody, tasks);
+  }
+
+  const finishTask = function(tasks, taskId) { tasks[taskId].pomodoroFinished = true; }
+  const increasePomodoro = function(tasks, taskId) { tasks[taskId].pomodoroDone++; }
+  const deleteTask = function(tasks, taskId) { tasks.splice(taskId, 1) }
+
+  pomodoroTableBody.addEventListener("click", handleTaskButtonClick);
   pomodoroForm.addEventListener("submit", addTask);
 
 })();
